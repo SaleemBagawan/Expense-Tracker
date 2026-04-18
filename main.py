da3f35f899d8d4973e1da259d11e2eb227cb5fdd
Saleem <saleemb.0223@gmail.com>
2026-04-19 01:21:51 +0530

    feat: add functional expense tracker with menu and input validation

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-expenses = []
 
-def add_expense(amount,category):
+
+def add_expense(amount,category,expenses):
     expense = {'amount':amount , 'category':category}
     expenses.append(expense)
+    return expenses
 
-def display_expenses():
+def display_expenses(expenses):
     if not expenses:
         print("No expense found")
         return 
     for record in expenses:
         print(f"Amount : {record['amount']}  Category : {record['category']}")
 
-def calculate_total():
+def calculate_total(expenses):
     total = 0
     for record in expenses:
         total += record['amount']
 
     return total
 
-def display_total(total):
-    print(f"Total expense : {total}")
 
-def display_expenses_above(limit):
+def display_expenses_above(limit,expenses):
     found = False
     for record in expenses:
         if record['amount'] >= limit:
@@ -31,15 +30,59 @@ def display_expenses_above(limit):
     if not found:
         print("No record found")
         
+def get_int_input(text):
+    while True:
+        try:
+            return int(input(text))
+        except ValueError:
+            print('please enter valid Number:')
+
+def get_string_input(text):
+    while True:
+        data = input(text)
+        if not data:
+            print('Enter can not be empty, please enter valid entry')
+        else:
+            return data
     
 
 
-add_expense(100,'Grocery')
-add_expense(120,'Food')
-add_expense(170,'Electricity Bill')
-add_expense(190,'Room rent')
 
-display_expenses()
-display_total(calculate_total())
-display_expenses_above(500)
+def main ():
+    expenses = []
+
+    while True:
+        print('*' * 20)
+        print('Please select the Operation :')
+        try:
+            choice = int(input("""
+            1) Add expense
+            2) Display expenses
+            3) Disply total
+            4) Disply expense above the limit
+            5) Exit
+            Please enter the choice (1/2/3/4/5) : """))
+        except ValueError:
+            print("Please enter a valid choice:")
+            continue
+        print('*' * 20)
+
+        if choice == 1:
+            amount = get_int_input('Please enter the amount:')
+            category = get_string_input('Please enter the category:')
+            add_expense(amount,category,expenses)
+            print('Expense added successfully:')
+        elif choice == 2:
+            display_expenses(expenses)
+        elif choice == 3:
+            print(f'Total expense = {calculate_total(expenses)}')
+        elif choice == 4:
+            limit = get_int_input('Enter the limit:')
+            display_expenses_above(limit,expenses)
+        else:
+            break
+
+main()
+
+
 
