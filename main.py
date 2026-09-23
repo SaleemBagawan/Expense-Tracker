08e3b81107c4ed1184b2c8619ac6d6e96a45b4e2
Saleem <saleemb.0223@gmail.com>
2026-09-24 00:45:43 +0530

    added middleware for CORS

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routers import expenses
 from database import Base,engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(expenses.router)
